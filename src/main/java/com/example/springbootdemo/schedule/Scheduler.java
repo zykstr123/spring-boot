7dcc33f3815d64303e85bc20ba39941411877c9f
zhouyankai <724172189@qq.com>
2020-06-18 19:54:35 +0800

    ??

--- src/main/java/com/example/springbootdemo/schedule/Scheduler.java
+++ src/main/java/com/example/springbootdemo/schedule/Scheduler.java
@@ -11,7 +11,7 @@ public class Scheduler {
     // 每隔1分钟刷新一次
     //被@PostConstruct修饰的方法会在服务器加载Servlet的时候运行
     @PostConstruct
-    @Scheduled(fixedRate = 10 * 1000)
+    @Scheduled(fixedRate = 60 * 1000)
     public void testTask() {
         System.out.println("-----------");
     }
