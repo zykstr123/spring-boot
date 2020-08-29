983ca58f4cc29f67cea38fd9adfa0c9e9c001e3a
zhouyankai <724172189@qq.com>
2020-08-29 15:31:47 +0800

    pooling

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -1,18 +1,27 @@
 package com.example.springbootdemo;
 
 import com.example.springbootdemo.reactor.GreetingWebClient;
+import com.example.springbootdemo.redis.RedisApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @SpringBootApplication
 @EnableScheduling
 public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(SpringBootDemoApplication.class, args);
-
+		//  I/O error on POST request for "http://localhost:9411/api/v2/spans"   zipkin没启动
 		GreetingWebClient gwc = new GreetingWebClient();
+		try {
+			RedisApi.addLink("1", new URL("www.baidu.com"));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 		System.out.println(gwc.getResult());
 	}
 }
--- src/main/java/com/example/springbootdemo/redis/RedisApi.java
+++ src/main/java/com/example/springbootdemo/redis/RedisApi.java
@@ -1,24 +1,34 @@
 package com.example.springbootdemo.redis;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.net.URL;
 
+@Component
+@Slf4j
 public class RedisApi {
-    @Autowired
-    private ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+
+    private static ReactiveRedisTemplate<String, Object> reactiveRedisTemplate;
+
+    @Resource
+    public void setReactiveRedisTemplate(ReactiveRedisTemplate<String, Object> reactiveRedisTemplate) {
+        RedisApi.reactiveRedisTemplate = reactiveRedisTemplate;
+        ReactiveRedisConnectionFactory redisConnectionFactory = reactiveRedisTemplate.getConnectionFactory();
+    }
 
     // inject the template as ListOperations
     // can also inject as Value, Set, ZSet, and HashOperations
-    @Resource(name="redisTemplate")
-    private ListOperations<String, String> listOps;
+//    @Resource(name="redisTemplate")
+//    private ListOperations<String, Object> listOps;
 
-    public void addLink(String userId, URL url) {
-        listOps.leftPush(userId, url.toExternalForm());
+    public static void addLink(String userId, URL url) {
+//        listOps.leftPush(userId, url.toExternalForm());
         // or use template directly
         reactiveRedisTemplate.opsForList().leftPush(userId, url.toExternalForm());
     }
