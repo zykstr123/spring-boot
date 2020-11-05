c084200fc74746c648af6ff1342c72e50b69e979
zhouyankai <724172189@qq.com>
2020-11-05 16:01:08 +0800

    try jackson

---
+++ src/main/java/com/example/springbootdemo/json/JsonTry.java
@@ -0,0 +1,38 @@
+package com.example.springbootdemo.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonTry {
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "Norman");
+        map.put("age", 25);
+        ObjectMapper objectMapper = new ObjectMapper();
+        People people = objectMapper.convertValue(map, People.class);
+        System.out.println(people.getAge());
+    }
+}
+
+class People {
+    private String name;
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
